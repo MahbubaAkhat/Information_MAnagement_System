375c0c626370b535ab5eaa8ffb5bc1a82b2b8643
Umme Mahbuba Jidni <76902093+MahbubaAkhat@users.noreply.github.com>
2022-07-07 23:43:46 +0600

    Add files via upload

---
+++ Information_MAnagement_System/src/Informaton_MAnagement_System/InformationClass.java
@@ -0,0 +1,22 @@
+package Informaton_MAnagement_System;
+
+
+public class InformationClass {
+    
+    String name;
+    String home_address ;
+    String country ;
+    String gender ;
+    String degree ;
+
+    InformationClass()
+    {
+      
+        
+        
+    }
+    
+    
+    
+}
+
